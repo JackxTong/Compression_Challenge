042668b4543fe54a61410aa2ba6c16e258cc38fa
Harry Allen <hha21@hurricane.ae.ic.ac.uk>
2025-01-27 20:35:09 +0000

    refactored code

---
+++ Curr/findProbabilities.h
@@ -0,0 +1,18 @@
+#ifndef FINDPROBABILITIES_H
+#define FINDPROBABILITIES_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <unordered_map>
+#include <filesystem>
+#include <vector>
+#include <utility> // For std::pair
+#include <memory>  // For std::unique_ptr
+
+// Function declarations
+
+#endif // FINDPROBABILITIES_H
